1e1b1f8e99bbc79971fd37a464b1d56535e489f0
evandro37854-maker <evandro37854@gmail.com>
2025-12-04 08:29:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,3 @@
-// ===============================
-<script>
-document.addEventListener("mousemove", (e) => {
-  const brilho = document.createElement("div");
-  brilho.className = "rastro";
-  brilho.style.left = e.pageX + "px";
-  brilho.style.top = e.pageY + "px";
-  document.body.appendChild(brilho);
-
-  setTimeout(() => {
-    brilho.remove();
-  }, 300);
-});
-</script>
-
-// ===============================
-<canvas id="particles"></canvas>
-
-<script>
-const canvas = document.getElementById("particles");
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particles = [];
-
-for (let i = 0; i < 80; i++) {
-  particles.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    r: Math.random() * 2 + 1,
-    dx: Math.random() * 0.5,
-    dy: Math.random() * 0.5
-  });
-}
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  particles.forEach(p => {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
-    ctx.fillStyle = "#00ffcc";
-    ctx.fill();
-
-    p.x += p.dx;
-    p.y += p.dy;
-
-    if (p.x > canvas.width) p.x = 0;
-    if (p.y > canvas.height) p.y = 0;
-  });
-
-  requestAnimationFrame(animate);
-}
-
-animate();
-</script>
 
 // ===== PEGAR O DIV DO FUNDO =====
 let fundo = document.getElementById('fundoBolas');
@@ -125,5 +67,6 @@ window.addEventListener('load', () => {
     setTimeout(() => loader.remove(), 800);
   }, 800);
 });
+
 
 
